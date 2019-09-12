9fafbde03ace0d865b3e6fdbbf4ed1c7df8299c2
James Aimonetti <james.aimonetti@gmail.com>
2019-09-12 22:18:01 +0000

    add skeleton app and Makefile

---
+++ src/matrices.hrl
@@ -0,0 +1,12 @@
+-ifndef(MATRICES_HRL).
+-include_lib("kazoo_stdlib/include/kz_types.hrl").
+-include_lib("kazoo_stdlib/include/kz_log.hrl").
+-include_lib("kazoo_stdlib/include/kz_databases.hrl").
+
+-define(APP_NAME, <<"matrices">>).
+-define(APP_VERSION, <<"0.0.1">>).
+
+-define(CACHE_NAME, 'matrices_cache').
+
+-define(MATRICES_HRL, 'true').
+-endif.
