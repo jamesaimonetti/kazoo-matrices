d869a98b74577bceadc64d232548309c4f87af4e
James Aimonetti <james.aimonetti@gmail.com>
2019-09-16 18:32:38 +0000

    canonicalize JSON

--- src/matrices_json.erl
+++ src/matrices_json.erl
@@ -13,7 +13,31 @@
 -include("matrices.hrl").
 
 -spec encode(kz_json:object()) -> iodata().
-encode(JObj) -> kz_json:encode(JObj).
+encode(JObj) ->
+    kz_json:encode(canonicalize(JObj)).
 
 -spec decode(iodata()) -> kz_json:object().
-decode(JSON) -> kz_json:decode(JSON).
+decode(JSON) ->
+    case re:run(JSON, <<"(u[a-fA-F0-9]{4})|(U+[a-fA-F0-9]{4})">>, [{'capture', 'all_but_first', 'binary'}, 'global']) of
+        {'match', Codepoints} -> decode(JSON, Codepoints);
+        'nomatch' -> kz_json:decode(JSON)
+    end.
+
+decode(JSON, CodePoints) ->
+    CleanedUp = lists:foldl(fun convert_point/2, JSON, CodePoints),
+    kz_json:decode(CleanedUp).
+
+convert_point(<<"\u", PointBin/binary>>=P, JSON) ->
+    replace(JSON, P, PointBin);
+convert_point(<<"U+", PointBin/binary>>=P, JSON) ->
+    replace(JSON, P, PointBin);
+convert_point([P], JSON) ->
+    convert_point(P, JSON).
+
+replace(JSON, P, PointBin) ->
+    Point = binary_to_integer(PointBin, 16),
+    Bin = unicode:characters_to_binary([Point]),
+    binary:replace(JSON, P, Bin).
+
+canonicalize(JObj) ->
+    kz_json:from_map(kz_json:to_map(JObj)).
--- test/matrices_json_test.erl
+++ test/matrices_json_test.erl
@@ -6,14 +6,14 @@
 -define(ONE_TWO, <<"{
     \"one\": 1,
     \"two\": \"Two\"
-}">>).
+}"/utf8>>).
 
 -define(B_A_PRETTY, <<"{
     \"b\": \"2\",
     \"a\": \"1\"
-}">>).
+}"/utf8>>).
 
--define(B_A_COMPACT, <<"{\"b\":\"2\",\"a\":\"1\"}">>).
+-define(B_A_COMPACT, <<"{\"b\":\"2\",\"a\":\"1\"}"/utf8>>).
 
 -define(AUTH_PRETTY, <<"{
     \"auth\": {
@@ -33,38 +33,43 @@
             ]
         }
     }
-}">>).
+}"/utf8>>).
 
 -define(A_GLYPH, <<"{
     \"a\": \"日本語\"
-}">>).
+}"/utf8>>).
 
 -define(GLYPH_1_2, <<"{
     \"本\": 2,
     \"日\": 1
-}">>).
+}"/utf8>>).
 
 -define(SLASH_U, <<"{
     \"a\": \"\u65E5\"
-}
-">>).
+}"/utf8>>).
 
 -define(A_NULL, <<"{
     \"a\": null
 }
-">>).
+"/utf8>>).
 
 canonical_test_() ->
-    Tests = [{<<"{}">>, <<"{}">>}
-            ,{?ONE_TWO, <<"{\"one\":1,\"two\":\"Two\"}">>}
-            ,{?B_A_PRETTY, <<"{\"a\":\"1\",\"b\":\"2\"}">>}
-            ,{?B_A_COMPACT, <<"{\"a\":\"1\",\"b\":\"2\"}">>}
-            ,{?AUTH_PRETTY, <<"{\"auth\":{\"mxid\":\"@john.doe:example.com\",\"profile\":{\"display_name\":\"John Doe\",\"three_pids\":[{\"address\":\"john.doe@example.org\",\"medium\":\"email\"},{\"address\":\"123456789\",\"medium\":\"msisdn\"}]},\"success\":true}}">>}
-            ,{?A_GLYPH, <<"{\"a\":\"日本語\"}">>}
-            ,{?GLYPH_1_2, <<"{\"日\":1,\"本\":2}">>}
-            ,{?SLASH_U, <<"{\"a\":\"日\"}">>}
-            ,{?A_NULL, <<"{\"a\":null}">>}
+    Tests = [{<<"{}"/utf8>>, <<"{}"/utf8>>}
+            ,{?ONE_TWO, <<"{\"one\":1,\"two\":\"Two\"}"/utf8>>}
+            ,{?B_A_PRETTY, <<"{\"a\":\"1\",\"b\":\"2\"}"/utf8>>}
+            ,{?B_A_COMPACT, <<"{\"a\":\"1\",\"b\":\"2\"}"/utf8>>}
+            ,{?AUTH_PRETTY, <<"{\"auth\":{\"mxid\":\"@john.doe:example.com\",\"profile\":{\"display_name\":\"John Doe\",\"three_pids\":[{\"address\":\"john.doe@example.org\",\"medium\":\"email\"},{\"address\":\"123456789\",\"medium\":\"msisdn\"}]},\"success\":true}}"/utf8>>}
+            ,{?A_GLYPH, <<"{\"a\":\"日本語\"}"/utf8>>}
+            ,{?GLYPH_1_2, <<"{\"日\":1,\"本\":2}"/utf8>>}
+            ,{?SLASH_U, <<"{\"a\":\"日\"}"/utf8>>}
+            ,{?A_NULL, <<"{\"a\":null}"/utf8>>}
             ],
-    [?_assertMatch(Canonical, matrices_json:encode(matrices_json:decode(Original)))
+    [canonicalize(Original, Canonical)
      || {Original, Canonical} <- Tests
     ].
+
+canonicalize(Original, Canonical) ->
+    TestName = lists:flatten(io_lib:format("~ts", [Canonical])),
+    Decoded = matrices_json:decode(Original),
+    RoundTrip = matrices_json:encode(Decoded),
+    {TestName, ?_assertEqual(Canonical, RoundTrip)}.
